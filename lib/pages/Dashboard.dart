ec9a38c5d0a605c1dc298d988972d652dec02603
Aditya Kumar <adityak007mr@gmail.com>
2020-07-26 15:12:47 +0530

    updated UI

--- lib/pages/Dashboard.dart
+++ lib/pages/Dashboard.dart
@@ -1,8 +1,10 @@
 import 'package:numometer/components/CountrySearch.dart';
+import 'package:numometer/components/DataList.dart';
 import 'package:numometer/components/LocaleData.dart';
 import 'package:numometer/components/Tracker.dart';
 import 'package:flutter/material.dart';
 import 'package:share/share.dart';
+import 'package:numometer/extras/SDApi.dart';
 
 class Dashboard extends StatefulWidget {
   Dashboard({Key key}) : super(key: key);
@@ -13,10 +15,12 @@ class Dashboard extends StatefulWidget {
 class _DashboardState extends State<Dashboard> {
   String display;
   int _selectedIndex = 0;
+  bool _lights = false;
   var shareURL = "https://play.google.com/store/apps/details?id=com.app.numometer";
   //var shareURL = loadYaml("/configs.yaml");
   static List<Widget> _widgetOptions = <Widget>[
     Tracker(),
+    DataList(),
     CountrySearchProvider(),
     LocaleDataProvider()
   ];
@@ -39,11 +43,22 @@ class _DashboardState extends State<Dashboard> {
     }); */
   }
 
+  invokeSocialDistancingAPI() {
+    SDApi invoke = new SDApi();
+    if(_lights){
+      invoke.invokeSocDistance();
+      var mon = invoke.monitorApi();
+      print(mon.toString());
+    }
+    else
+      invoke.stopTransmission();
+  }
+
   final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      key: scaffoldKey,
+    return 
+    Scaffold(
       appBar: AppBar(
         title: const Text('NUMOMETER'),
         centerTitle: true,
@@ -53,7 +68,7 @@ class _DashboardState extends State<Dashboard> {
             onPressed: () {
               final RenderBox box = context.findRenderObject();
               Share.share("Check out this new App : " + shareURL,
-                  subject: "CVKavach",
+                  subject: "NumOMeter",
                   sharePositionOrigin:
                       box.localToGlobal(Offset.zero) & box.size);
             },
@@ -61,24 +76,66 @@ class _DashboardState extends State<Dashboard> {
         ],
       ),
       body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
+        child: Scaffold(
+          key: scaffoldKey,
+          body: Center(
+            child: _widgetOptions.elementAt(_selectedIndex),
+          ),
+          bottomNavigationBar: BottomNavigationBar(
+              items: const <BottomNavigationBarItem>[
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.language),
+                  title: Text('Tracker'),
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.assignment),
+                  title: Text('Full List')
+                ),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.search), title: Text('Search')),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.remove_red_eye), title: Text('Locale Data'))
+              ],
+              currentIndex: _selectedIndex,
+              selectedItemColor: Colors.amber[800],
+              onTap: _onItemTapped,
+              backgroundColor: Color(0xFF101010),
+              type: BottomNavigationBarType.fixed),
+        )
       ),
-      bottomNavigationBar: BottomNavigationBar(
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(Icons.language),
-              title: Text('Tracker'),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text('Drawer Header'),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
             ),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.search), title: Text('Search')),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.remove_red_eye), title: Text('Locale Data'))
-          ],
-          currentIndex: _selectedIndex,
-          selectedItemColor: Colors.amber[800],
-          onTap: _onItemTapped,
-          backgroundColor: Color(0xFF101010),
-          type: BottomNavigationBarType.fixed),
+            SwitchListTile(
+              title: const Text('Enable Social Distancing API'),
+              value: _lights,
+              onChanged: (bool value) {
+                setState(() {
+                  _lights = value;
+                });
+                invokeSocialDistancingAPI();
+              },
+              secondary: const Icon(Icons.blur_circular),
+            ),
+            ListTile(
+              title: Text('Item 2'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+          ]
+        )
+      ),
     );
   }
 }
