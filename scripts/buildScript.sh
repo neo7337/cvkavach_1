4ccb1a67d405baec521de2e8cc3070e0e1d3ab17
Aditya Kumar <adityak007mr@gmail.com>
2020-07-27 17:38:09 +0530

    custom-workflows

--- scripts/buildScript.sh
+++ scripts/buildScript.sh
@@ -4,11 +4,16 @@
 # To be used only in case of SNAPSHOT or RELEASE upload to dev, test or master branches
 #
 
-if [$# -lt 4]: then
+set -e
+
+if [ $# -lt 4 ];
+then
     echo "Invalid Number of arguments"
     echo "Please enter the <build-version> <commit-message> <Build-Type eg: SNAPSHOT or RELEASE> <branch>"
     exit 1
 fi
+
+cd ..
 
 # commit the build to github
 git add .
@@ -21,6 +26,7 @@ git push -u origin $4
 # add tags to the git
 
 TAG_NAME = "$1:$3"
+echo "TAG NAME", $TAG_NAME
 git tag -f $TAG_NAME -m $2
 
 git push —tags -f
